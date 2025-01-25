c47031da47726981942453328f360d0ce5a167b1
Anton714 <an714.am@gmail.com>
2025-01-25 14:15:01 +0500

    Add files via upload
    Использование БД в маршрутизации. 1.1

---
+++ app/routers/task.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status, HTTPException, Path
+from sqlalchemy.orm import Session
+from app.backend.db_depends import get_db
+from typing import Annotated
+from app.models.user import User
+from app.schemas import CreateUser, UpdateUser
+from sqlalchemy import insert, select, update, delete
+from slugify import slugify
+router = APIRouter(prefix="/task", tags=["task"])
+
+@router.get("/")
+async def all_tasks():
+    pass
+
+@router.get("/task_id")
+async def task_by_id():
+    pass
+
+@router.post("/create")
+async def create_task():
+    pass
+
+@router.put("/update")
+async def update_task():
+    pass
+
+@router.delete("/delete")
+async def delete_task():
+    pass
